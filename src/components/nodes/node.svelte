e7a90d799f239ab6be513d3739834ea7a1338d5a
Dan Martinez <jeht05@gmail.com>
2024-06-08 02:12:37 -0500

    Trying svelte out.

---
+++ src/components/nodes/node.svelte
@@ -0,0 +1,10 @@
+<div class="node-container"></div>
+
+<style>
+    .node-container {
+        background-color: black;
+        width: 300px;
+        height: 100px;
+        margin: 20px;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang='ts'>
+    import NodeList from '../components/nodes/node-list.svelte';
+</script>
+
+<NodeList></NodeList>
+
+<style>
+
+</style>
