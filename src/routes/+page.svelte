f23cf1dad50d745d1317126aaa481ac173c2382c
Dan Martinez <jeht05@gmail.com>
2024-06-10 21:00:44 -0500

    Trying out different time components.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import NodeList from '$lib/nodes/node-list.svelte';
 	import Clock from '$lib/clock.svelte';
+	import CountdownBar from '$lib/countdown-bar.svelte';
+	import TimeLineBar from '$lib/time-line-bar.svelte';
 </script>
 
+<TimeLineBar></TimeLineBar>
+<CountdownBar></CountdownBar>
 <Clock></Clock>
 <NodeList></NodeList>
-
-<style>
-</style>
