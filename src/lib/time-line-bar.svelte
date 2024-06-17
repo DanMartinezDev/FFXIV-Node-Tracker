9f76dd569cbeedb84e5868c50d7af217bd0f6bb0
Dan Martinez <jeht05@gmail.com>
2024-06-16 19:57:26 -0500

    Tinkering.

--- src/lib/time-line-bar.svelte
+++ src/lib/time-line-bar.svelte
@@ -1,25 +1,23 @@
 <script lang="ts">
 	let spacing = 10;
+	let duration = '.1s';
 </script>
 
 <div class="time-line-container">
-	{#each { length: 60 } as _, ticks}
-		<div class="time-tick" style="--start: {ticks * spacing}px; --distance: {ticks * spacing - spacing}px; --duration: 1s;">|</div>
+	{#each { length: 101 } as _, ticks}
+		<div class="time-tick" style="--start: {ticks * spacing}px; --distance: {ticks * spacing - spacing}px; --duration: {duration};">|</div>
 	{/each}
 </div>
 
-<br />
-
-<br />
-
 <style>
 	.time-line-container {
+		position: relative;
 		border: thin solid #fff;
 		padding-bottom: 23px;
+		width: 1000px;
 	}
 
 	.time-tick {
-		margin-right: 0;
 		padding: 0px;
 		animation-name: time-tick-keyframe;
 		position: absolute;
